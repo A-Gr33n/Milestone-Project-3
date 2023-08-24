4b3b58ffd50026de24540c13e99c6028524ad127
A-Gr33n <aarongreen193@hotmail.com>
2023-08-24 22:33:49 +0000

    added modal to delete function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,10 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+  
+   var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+  
   });
 
 
-  var instance = M.Carousel.init({
-    fullWidth: true,
-    indicators: true
+  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.modal');
+    M.Modal.init(options);
   });
