1e63adb2ce482ee99b54827b720f0222b6be377b
A-Gr33n <aarongreen193@hotmail.com>
2023-08-25 18:01:14 +0000

    reajusted image and card size

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    M.Carousel.init(elems, {indicators: true});
+    M.Carousel.init(elems,  {indicators: true});
   });
 
   
