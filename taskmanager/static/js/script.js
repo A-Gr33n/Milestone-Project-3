b001bec5f813bdb00fdf6cf5054482b8d911113d
A-Gr33n <aarongreen193@hotmail.com>
2023-08-25 17:37:23 +0000

    Added carousel images and indicators to main page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -7,8 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    M.Carousel.init(elems, {});
+    M.Carousel.init(elems, {indicators: true});
   });
+
+  
 
   
 
