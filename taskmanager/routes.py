e52dc7bc5613f776ccdcba94a61c94400a5aa3d8
A-Gr33n <aarongreen193@hotmail.com>
2023-09-07 14:18:24 +0000

    updated routes.py for testing

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,34 +1,42 @@
 from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Recipe
+
+
 # Home
 @app.route("/")
 def home():
     return render_template("main.html")
+
+
 # Recipe
 @app.route("/recipes")
 def recipes():
     recipes = list(Recipe.query.order_by(Recipe.recipe_name).all())
     return render_template("recipes.html", recipes=recipes)
+
+
 # Add recipe
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     print(request)
     if request.method == "POST":
         recipe = Recipe(
-            recipe_name = request.form.get("recipe_name"),
-            recipe_description = request.form.get("recipe_description"),
-            ingredients = request.form.get("ingredients"),
-            cook_time = request.form.get("cook_time"),
-            prep_time= request.form.get("perp_time"),
-            servings = request.form.get("servings"),
-            difficulty = request.form.get("difficulty"),
-            image_url = request.form.get("image_url")
+            recipe_name=request.form.get("recipe_name"),
+            recipe_description=request.form.get("recipe_description"),
+            ingredients=request.form.get("ingredients"),
+            cook_time=request.form.get("cook_time"),
+            prep_time=request.form.get("perp_time"),
+            servings=request.form.get("servings"),
+            difficulty=request.form.get("difficulty"),
+            image_url=request.form.get("image_url")
         )
         db.session.add(recipe)
         db.session.commit()
         return redirect(url_for("recipes"))
     return render_template("add_recipe.html")
+
+
 # Edit Recipe
 @app.route("/edit_recipe/<int:recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
@@ -45,6 +53,8 @@ def edit_recipe(recipe_id):
         db.session.commit()
         return redirect(url_for("recipes"))
     return render_template("edit_recipe.html", recipe=recipe)
+
+
 # Delete Recipe
 @app.route("/delete_recipe/<int:recipe_id>")
 def delete_recipe(recipe_id):
@@ -52,3 +62,4 @@ def delete_recipe(recipe_id):
     db.session.delete(recipe)
     db.session.commit()
     return redirect(url_for("recipes"))
+
